9a02722bac686ee463d03295079a19535479e8b5
Rostislav-Myasnikov <operkaka4@gmail.com>
2025-09-21 17:34:47 +0300

    testsvg

--- src/js/markup-comment.js
+++ src/js/markup-comment.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
+import 'starability/starability-css/starability-all.css';
 
 export function getAPI() {
   axios.defaults.baseURL = 'https://paw-hut.b.goit.study/';
@@ -23,8 +24,69 @@ export function markupComment(data) {
     .map(res => {
       return `
         <div class="swiper-slide swiper-comment-slide">
-          <p class="description-comment">${res.description}</p>
-          <p class='author-comment'>${res.author}</p>
+  <div class="rating value-${res.rate} star-svg half small">
+    <div class="star-container">
+      <div class="star">
+            <svg class="star-empty">
+                <use href="/star-rating.icons.svg#star-empty"></use>
+            </svg>
+            <svg class="star-half">
+                <use href="/star-rating.icons.svg#star-half"></use>
+            </svg>
+            <svg class="star-filled">
+                <use href="/star-rating.icons.svg#star-filled"></use>
+            </svg>
+        </div>
+      <div class="star">
+        <svg class="star-empty" width="20px" height="19px">
+          <use href="../svg/icons.svg#icon-star-outline"></use>
+        </svg>
+        <svg class="star-half" width="20px" height="19px">
+          <use href="../svg/icons.svg#icon-star-half"></use>
+        </svg>
+        <svg class="star-filled" width="20px" height="19px">
+          <use href="../svg/icons.svg#icon-star-filled"></use>
+        </svg>
+      </div>
+        <div class="star">
+        <svg class="star-empty" width="20px" height="19px">
+          <use href="../svg/icons.svg#icon-star-outline"></use>
+        </svg>
+        <svg class="star-half" width="20px" height="19px">
+          <use href="../svg/icons.svg#icon-star-half"></use>
+        </svg>
+        <svg class="star-filled" width="20px" height="19px">
+          <use href="../svg/icons.svg#icon-star-filled"></use>
+        </svg>
+      </div>
+         <div class="star">
+        <svg class="star-empty" width="20px" height="19px">
+          <use href="../svg/icons.svg#icon-star-outline"></use>
+        </svg>
+        <svg class="star-half" width="20px" height="19px">
+          <use href="../svg/icons.svg#icon-star-half"></use>
+        </svg>
+        <svg class="star-filled" width="20px" height="19px">
+          <use href="../svg/icons.svg#icon-star-filled"></use>
+        </svg>
+      </div>
+            <div class="star">
+        <svg class="star-empty" width="20px" height="19px">
+          <use href="../svg/icons.svg#icon-star-outline"></use>
+        </svg>
+        <svg class="star-half" width="20px" height="19px">
+          <use href="../svg/icons.svg#icon-star-half"></use>
+        </svg>
+        <svg class="star-filled" width="20px" height="19px">
+          <use href="../svg/icons.svg#icon-star-filled"></use>
+        </svg>
+      </div>
+            </div>
+          </div>
+          <div>
+            <p class="description-comment">${res.description}</p>
+            <p class="author-comment">${res.author}</p>
+          </div>
         </div>
       `;
     })
@@ -33,6 +95,7 @@ export function markupComment(data) {
 }
 
 getAPI().then(data => {
+  console.log(data);
   markupComment(data);
 
   new Swiper('.stories-swiper', {
@@ -46,6 +109,7 @@ getAPI().then(data => {
     pagination: {
       el: '.stories-pagination',
       clickable: true,
+      dynamicBullets: true,
     },
     breakpoints: {
       768: {
